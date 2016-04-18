c7552f384ce244944a0fc9c30840ca9816f4653f
lplit <lplit@wp.pl>
2016-04-18 02:02:10 +0200

    Gem functional

--- lib/phoenix5k.rb
+++ lib/phoenix5k.rb
@@ -1,5 +1,3 @@
-require "phoenix5k/version"
-
 # Require all of the Ruby files in the given directory.
 #
 # path - The String relative path from here to the directory.
@@ -8,12 +6,15 @@ require "phoenix5k/version"
 def require_all(path)
   glob = File.join(File.dirname(__FILE__), path, '*.rb')
   Dir[glob].each do |f|
+    puts "Adding " +f.to_s
     require f
   end
 end
 
 module Phoenix5k
-	require_all 'phoenix5k/deploy'
-	require_all 'phoenix5k/supervisor'
-	require_all 'phoenix5k/tools'
+  require "phoenix5k/version"
+  require "phoenix5k/toolbox"
+  require_all "phoenix5k/deploy"
+  require_all "phoenix5k/supervisor"
+
 end
