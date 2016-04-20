96401c1913f1a6e043861f3d75e79b4c10ef3c98
lplit <lplit@wp.pl>
2016-04-20 16:20:19 +0200

    Heartbeat

--- lib/phoenix5k/supervisor/antidoteMonitor.rb
+++ lib/phoenix5k/supervisor/antidoteMonitor.rb
@@ -2,7 +2,8 @@
 
      module Phoenix5k
      	class AntidoteMonitor
-
+     		@supervise=false 
+     		# Pings all antidote instances
      		def ping
      			command = "for d in #{$antidote_path}; do $d/bin/antidote ping; done"
      			deployment["nodes"].each do |host|
@@ -14,6 +15,26 @@
      				end
      			end
      		end
+
+     		# Periodically pings antidote and reports
+     		# @param [Int] freq Refresh rate in seconds 
+     		def heartbeat freq 
+     			while @supervise 
+     				ping
+     			end
+     		end
+
+     		# Stops the supervision daemon 
+    		# @return Nothing 
+    		def stop_d 
+    			@supervise = false
+    		end 
+
+		    # Stops and kills supervision
+		    def stopKill
+		      self.stop_d
+		      return
+		    end
 
      	end # Class
      end
